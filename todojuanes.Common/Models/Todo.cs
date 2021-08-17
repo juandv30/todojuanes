fb7b22717c360af188bd3811448e2ef5456e41eb
juandv30 <78824982+juandv30@users.noreply.github.com>
2021-08-16 21:55:21 -0500

    Add entity

---
+++ todojuanes.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todojuanes.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool isCompleted { get; set; }
+    }
+}
---
+++ todojuanes.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace todojuanes.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool isCompleted { get; set; }
+    }
+}
